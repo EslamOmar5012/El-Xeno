6aa4a98a0025af429df7df15923d390d07ce88fc
Eslam Omar <eslam.O.elkhabery@gmail.com>
2025-07-31 05:59:48 +0300

    finish work events section

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
 const progressBars = document.querySelectorAll(".bar");
 
 progressBars.forEach((bar) => {
-  console.log(bar);
   const value = bar.dataset.progress;
   bar.style.background = `linear-gradient(to right, #2196f3 ${value}, #ececec ${value})`;
 });
